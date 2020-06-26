855bdada85de4008922167d52f8e8babaa3e05c9
Teody C. Seguin <teodyseguin@gmail.com>
2020-06-27 00:46:23 +0800

    Add solution to sorting
    From this commit, I have added a function for email validation

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const path = require('path');
 // I have created this json file, based on the information gathered here https://www.nationsonline.org/oneworld/US-states-population.htm
 const states = require('./states.json');
+const _ = require('lodash');
 
 // This will contain the new set of records for generating a new csv file.
 let newRecord = [];
 
-fs.createReadStream(path.resolve(__dirname, '.', 'contacts.tsv'))
+fs.createReadStream(path.resolve(__dirname, '.', 'contacts.csv'))
   .pipe(fcsv.parse({ headers: true }))
   .on('error', error => console.error(error))
   .on('data', row => createNewRow(row))
@@ -21,6 +22,13 @@ const createNewRow = row => {
   const { Last, Email, State } = row;
   const found = states.find(state => state.state === State);
 
+  if (!validateEmail(Email)) {
+    return;
+  }
+
+  let emailChunks = Email.split('@');
+  let emailDomain = emailChunks[1];
+
   // If no matching state is found, we stop.
   if (!found) {
     return;
@@ -33,6 +41,9 @@ const createNewRow = row => {
 
   // A record that has less than 10 million population will be added to the new record.
   if (parseInt(found.population) < 10000000) {
+    // We are adding a temporary property named emailDomain.
+    // The purpose of this, is so that we can easily pass it as an argument for sorting later.
+    row['emailDomain'] = emailDomain;
     newRecord.push(row);
   }
 };
@@ -43,8 +54,24 @@ const createNewRow = row => {
  */
 const generateNewFile = rowCount => {
   console.log(`Parsed ${rowCount} rows`);
+  // Sort the record by email domain and last name.
+  newRecord = _.sortBy(newRecord, ['emailDomain', 'Last']);
+
+  // Once everything has been sorted, we are now going to remove the emailDomain property for each record object.
+  newRecord.forEach(element => {
+    delete element.emailDomain;
+  });
+
   fcsv.writeToPath(path.resolve(__dirname, 'new-contacts.csv'), newRecord)
     .on('error', error => console.log('Something went wrong while trying to generate new-contacts.csv', error))
     .on('finish', () => console.log('new-contacts.csv has been successfully generated'));
 };
+
+/**
+ * https://stackoverflow.com/a/46181
+ */
+const validateEmail = email => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
